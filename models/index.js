2368caab246bf9677b7d2308d1d72e35c9679356
Uchenna Obicheta <uobicheta@hotmail.com>
2023-03-17 17:55:54 -0400

    Updated index.js for models.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id',
+});
+
 
 module.exports = {
   Product,
