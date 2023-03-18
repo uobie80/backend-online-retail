e049c8d9eeadc3aa9d295d370dc71cf268f12941
Uchenna Obicheta <uobicheta@hotmail.com>
2023-03-17 21:59:11 -0400

    Updated product and tag routes.

--- models/index.js
+++ models/index.js
@@ -8,28 +8,42 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// Products belongToMany Tags (through ProductTag)
+// ProductTag belongsTo Product
 ProductTag.belongsTo(Product, {
   foreignKey: 'product_id',
 });
 
-Product.hasMany(ProductTag, {
-  foreignKey: 'product_id',
-});
-
-// Tags belongToMany Products (through ProductTag)
+// ProductTag belongsTo Tag
 ProductTag.belongsTo(Tag, {
   foreignKey: 'tag_id',
 });
 
+// Product has many ProductTag
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id',
+});
+
+// Tag has many ProductTag
 Tag.hasMany(ProductTag, {
   foreignKey: 'tag_id',
 });
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
+
 
 
 module.exports = {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,86 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+       include: [
+                    {
+                      model: Product
+                    }
+                 ]
+  }).then((tagData) => {
+    res.json(tagData);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, 
+    
+    {
+      include: [
+                   {
+                     model: Product
+                   }
+                ]
+     }
+
+      ).then((tagData) => {
+        res.json(tagData);
+      }).catch((err) => {
+        res.json(err);
+      });
+
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((newTagData) => {
+    res.json(newTagData);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      tag_name: req.body.tag_name,
+    },
+    {
+      // Gets the Tag based on the id given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((updatedTag) => {
+      // Sends the updated book as a json response
+      res.json(updatedTag);
+    }).catch((err) => res.json(err));
 });
+
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedTag) => {
+      res.json(deletedTag);
+    })
+    .catch((err) => res.json(err));
+
 });
 
 module.exports = router;
